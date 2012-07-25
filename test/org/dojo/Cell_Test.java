e304e0ed620abdf0ea323764c9ae849b820e3c3a
Andrew Oxenburgh <andrew.oxenburgh@adscale.co.nz>
2012-07-25 18:37:10 +1200

    fixed

--- test/org/dojo/Cell_Test.java
+++ test/org/dojo/Cell_Test.java
@@ -67,54 +67,62 @@ public class Cell_Test {
 
     @Test
     public void liveCellWithFewerThanTwoLiveNeighboursDies() throws Exception {
-        assertCellWithNeighborsNextStateEquals(ALIVE, 0, DEAD);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 1, DEAD);
+        assertCellWithNeighborsDies(ALIVE, 0);
+        assertCellWithNeighborsDies(ALIVE, 1);
     }
 
 
     @Test
     public void givenLiveCellWith2or3liveNeighbours() {
-        assertCellWithNeighborsNextStateEquals(ALIVE, 2, ALIVE);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 3, ALIVE);
+        assertCellWithNeighborsLives(ALIVE, 2);
+        assertCellWithNeighborsLives(ALIVE, 3);
     }
 
 
     @Test
     public void givenLiveCellWithMoreThan3neighboursDies() throws Exception {
-        assertCellWithNeighborsNextStateEquals(ALIVE, 4, DEAD);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 5, DEAD);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 6, DEAD);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 7, DEAD);
-        assertCellWithNeighborsNextStateEquals(ALIVE, 8, DEAD);
+        assertCellWithNeighborsDies(ALIVE, 4);
+        assertCellWithNeighborsDies(ALIVE, 5);
+        assertCellWithNeighborsDies(ALIVE, 6);
+        assertCellWithNeighborsDies(ALIVE, 7);
+        assertCellWithNeighborsDies(ALIVE, 8);
     }
 
 
     //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
     @Test
     public void givenADeadCellWithExactlyThreeNeighboursComesToLife() {
-        assertCellWithNeighborsNextStateEquals(DEAD, 3, ALIVE);
+        assertCellWithNeighborsLives(DEAD, 3);
     }
 
 
     @Test
     public void givenADeadCellThatDoesntHave3LiveNeighbours_staysDead() throws Exception {
-        assertCellWithNeighborsNextStateEquals(DEAD, 0, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 1, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 2, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 4, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 5, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 6, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 7, DEAD);
-        assertCellWithNeighborsNextStateEquals(DEAD, 8, DEAD);
+        assertCellWithNeighborsDies(DEAD, 0);
+        assertCellWithNeighborsDies(DEAD, 1);
+        assertCellWithNeighborsDies(DEAD, 2);
+        assertCellWithNeighborsDies(DEAD, 4);
+        assertCellWithNeighborsDies(DEAD, 5);
+        assertCellWithNeighborsDies(DEAD, 6);
+        assertCellWithNeighborsDies(DEAD, 7);
+        assertCellWithNeighborsDies(DEAD, 8);
     }
 
 
-    private void assertCellWithNeighborsNextStateEquals(CellState currentState, int liveNeighbours, CellState expectedState) {
+    private void assertCellWithNeighborsLives(CellState currentState, int liveNeighbours) {
         Cell cell = new Cell();
         cell.state = currentState;
         cell.neighboursAlive = liveNeighbours;
 
-        assertThat(cell.nextState(), is(expectedState));
+        assertThat(cell.nextState(), is(ALIVE));
+    }
+
+    private void assertCellWithNeighborsDies(CellState currentState, int liveNeighbours) {
+        Cell cell = new Cell();
+        cell.state = currentState;
+        cell.neighboursAlive = liveNeighbours;
+
+        assertThat(cell.nextState(), is(DEAD));
     }
 
 }
